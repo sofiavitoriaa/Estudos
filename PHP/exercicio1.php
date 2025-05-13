e4b50d8612e00f387f29085399266a50fb3142d9
sofiavitoriaa <sofiavitoria4331@gmail.com>
2025-05-13 20:33:18 -0300

    kk

--- PHP/exercicio1.php
+++ PHP/exercicio1.php
@@ -1,3 +1,12 @@
 <?php
     echo  "esse é o meu arquivo index";
+    echo '<h1> Arquivo index</h1>';
+    echo "texto dentro de aspas duplas";
+    echo "123456";
+
+    print 'texto usando print';
+    print "<small> texto dentro de aspas duplas </small>";
+    echo "<br>";
+
+    echo "<h1> Arquivo index </h1>";
 ?>
